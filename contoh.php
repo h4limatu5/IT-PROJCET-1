71c230c090622a5c67a835be8055cecb179500ee
unknown <siti.noor@mhs.politala.ac.id>
2025-09-02 15:03:57 +0800

    Saya menambahkan file baru contoh.php

--- contoh.php
+++ contoh.php
@@ -1,9 +1,16 @@
+<<<<<<< HEAD
 <!DOCTYPE html>
+=======
+>>>>>>> 32d29b9 (Saya menambahkan file baru contoh.php)
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<<<<<<< HEAD
     <title>selamat datang </title>
+=======
+    <title>selamat datang</title>
+>>>>>>> 32d29b9 (Saya menambahkan file baru contoh.php)
 </head>
 <body>
     
