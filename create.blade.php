9839a4172b4248a7eae33f83676be196dacd2f47
h4limatu5 <hsadiah683@gmail.com>
2025-10-22 20:55:51 +0800

    prodi

--- create.blade.php
+++ create.blade.php
@@ -6,46 +6,21 @@
         <div class="col-md-12">
             <div class="card">
                 <div class="card-header">
-                    <h3>Tambah Perusahaan</h3>
+                    <h3>Tambah Program Studi</h3>
                 </div>
                 <div class="card-body">
-                    <form action="{{ route('perusahaan.store') }}" method="POST" enctype="multipart/form-data">
+                    <form action="{{ route('prodi.store') }}" method="POST" enctype="multipart/form-data">
                         @csrf
                         <div class="form-group">
-                            <label for="nama_perusahaan">Nama Perusahaan *</label>
-                            <input type="text" class="form-control @error('nama_perusahaan') is-invalid @enderror" id="nama_perusahaan" name="nama_perusahaan" value="{{ old('nama_perusahaan') }}" required>
-                            @error('nama_perusahaan')
+                            <label for="nama_prodi">Nama Program Studi *</label>
+                            <input type="text" class="form-control @error('nama_prodi') is-invalid @enderror" id="nama_prodi" name="nama_prodi" value="{{ old('nama_prodi') }}" required>
+                            @error('nama_prodi')
                                 <div class="invalid-feedback">{{ $message }}</div>
                             @enderror
                         </div>
-                        <div class="form-group">
-                            <label for="alamat">Alamat *</label>
-                            <textarea class="form-control @error('alamat') is-invalid @enderror" id="alamat" name="alamat" required>{{ old('alamat') }}</textarea>
-                            @error('alamat')
-                                <div class="invalid-feedback">{{ $message }}</div>
-                            @enderror
-                        </div>
-                        <div class="form-group">
-                            <label for="provinsi">Provinsi</label>
-                            <input type="text" class="form-control @error('provinsi') is-invalid @enderror" id="provinsi" name="provinsi" value="{{ old('provinsi') }}">
-                            @error('provinsi')
-                                <div class="invalid-feedback">{{ $message }}</div>
-                            @enderror
-                        </div>
-                        <div class="form-group">
-                            <label for="telepon">Telepon *</label>
-                            <input type="text" class="form-control @error('telepon') is-invalid @enderror" id="telepon" name="telepon" value="{{ old('telepon') }}" required>
-                            @error('telepon')
-                                <div class="invalid-feedback">{{ $message }}</div>
-                            @enderror
-                        </div>
-                        <div class="form-group">
-                            <label for="email">Email *</label>
-                            <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email" value="{{ old('email') }}" required>
-                            @error('email')
-                                <div class="invalid-feedback">{{ $message }}</div>
-                            @enderror
-                        </div>
+
+
+
                         <div class="form-group">
                             <label for="deskripsi">Deskripsi</label>
                             <textarea class="form-control @error('deskripsi') is-invalid @enderror" id="deskripsi" name="deskripsi">{{ old('deskripsi') }}</textarea>
@@ -53,33 +28,18 @@
                                 <div class="invalid-feedback">{{ $message }}</div>
                             @enderror
                         </div>
+
                         <div class="form-group">
-                            <label for="logo">Logo Perusahaan</label>
-                            <input type="file" class="form-control @error('logo') is-invalid @enderror" id="logo" name="logo" accept="image/*">
-                            @error('logo')
+                            <label for="photo">Foto Program Studi</label>
+                            <input type="file" class="form-control @error('photo') is-invalid @enderror" id="photo" name="photo" accept="image/*">
+                            @error('photo')
                                 <div class="invalid-feedback">{{ $message }}</div>
                             @enderror
+                            <small class="form-text text-muted">Format: JPG, PNG, GIF. Maksimal 2MB.</small>
                         </div>
-                        <div class="form-group">
-                            <label>Program Studi</label>
-                            <div class="row">
-                                @foreach($prodis as $prodi)
-                                <div class="col-md-6">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" id="prodi_{{ $prodi->id }}" name="prodi_ids[]" value="{{ $prodi->id }}" {{ in_array($prodi->id, old('prodi_ids', [])) ? 'checked' : '' }}>
-                                        <label class="form-check-label" for="prodi_{{ $prodi->id }}">
-                                            {{ $prodi->nama_prodi }}
-                                        </label>
-                                    </div>
-                                </div>
-                                @endforeach
-                            </div>
-                            @error('prodi_ids')
-                                <div class="invalid-feedback d-block">{{ $message }}</div>
-                            @enderror
-                        </div>
+
                         <button type="submit" class="btn btn-primary">Simpan</button>
-                        <a href="{{ route('perusahaan.index') }}" class="btn btn-secondary">Kembali</a>
+                        <a href="{{ route('prodi.index') }}" class="btn btn-secondary">Kembali</a>
                     </form>
                 </div>
             </div>
